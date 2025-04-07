3865d86f1e49eee4aa328ccc9fb75b51809cee03
longtongster <sacha.van.weeren@gmail.com>
2025-04-07 13:54:00 +0200

    contains all files and steps for a simple ml project

---
+++ utils_and_constants.py
@@ -0,0 +1,14 @@
+DATASET_TYPES = ["test", "train"]
+DROP_COLNAMES = ["row ID"]
+TARGET_COLUMN = "RainTomorrow"
+RAW_DATASET = "./raw_dataset/weather_training_data.csv"
+PROCESSED_DATASET = "./processed_dataset/weather.csv"
+
+
+def delete_and_recreate_dir(path):
+    try:
+        shutil.rmtree(path)
+    except:
+        pass
+    finally:
+        Path(path).mkdir(parents=True, exist_ok=True)
